9a1368042dbba8807f3ec371bbe44ad31355c634
TaykYoku <yokutayk@gmail.com>
2020-01-13 16:42:24 +0100

    Use username to get proxy

--- OAuthDIRAC/FrameworkSystem/Utilities/ProxyHandler.py
+++ OAuthDIRAC/FrameworkSystem/Utilities/ProxyHandler.py
@@ -8,8 +8,8 @@ from tornado import web, gen
 from tornado.template import Template
 
 from DIRAC import S_OK, S_ERROR, gConfig, gLogger
-from DIRAC.ConfigurationSystem.Client.Helpers import Registry
 from DIRAC.FrameworkSystem.Client.ProxyManagerClient import ProxyManagerClient
+from DIRAC.ConfigurationSystem.Client.Helpers.Registry import getDNForUsernameInGroup
 
 from WebAppDIRAC.Lib.WebHandler import WebHandler, asyncGen, WErr
 
@@ -36,14 +36,14 @@ class ProxyHandler(WebHandler):
     """ Proxy management endpoint, use:
           GET /proxy?<options> -- retrieve personal proxy
             * options:
-              * voms - VOMS name(optional)
+              * voms - to get VOMSproxy(optional)
               * lifetime - requested proxy live time(optional)
 
-          GET /proxy/<username>/<group>?<options> -- retrieve proxy
-            * username - user name
+          GET /proxy/<user>/<group>?<options> -- retrieve proxy
+            * user - user name
             * group - group name
             * options:
-              * voms - VOMS name(optional)
+              * voms - to get VOMSproxy(optional)
               * lifetime - requested proxy live time(optional)
 
           GET /proxy/metadata?<options> -- retrieve proxy metadata..
@@ -64,8 +64,8 @@ class ProxyHandler(WebHandler):
 
       # Return personal proxy
       elif not self.overpath:
-        result = yield self.threadTask(ProxyManagerClient().downloadPersonalProxy, self.getDN(), self.getUserGroup(), requiredTimeLeft=proxyLifeTime,
-                                                     vomsAttr=Registry.getVOForGroup(group) if voms else None)
+        result = yield self.threadTask(ProxyManagerClient().downloadPersonalProxy, self.getUserName(),
+                                       self.getUserGroup(), requiredTimeLeft=proxyLifeTime, vomsAttr=voms)
         if not result['OK']:
           raise WErr(500, result['Message'])
         self.log.notice('Proxy was created.')
@@ -76,33 +76,18 @@ class ProxyHandler(WebHandler):
 
       # Return proxy
       elif len(optns) == 2:
-        username = optns[0]
+        user = optns[0]
         group = optns[1]
         
-        # # Need group to continue
-        # if not group:
-        #   result = Registry.findDefaultGroupForUser(userName)
-        #   if not result['OK']:
-        #     raise WErr(500, result['Message'])
-        #   group = result['Value']
-        # result = Registry.getGroupsForUser(userName)
-        # if not result['OK']:
-        #   raise WErr(500, result['Message'])
-        # elif group not in result['Value']:
-        #   raise WErr(500, '%s group is not found for %s user.' % (group, userName))
-        
         # Get proxy to string
-        result = Registry.getDNForUsernameInGroup(username, group)
-        if not result['OK']:
-          raise WErr(500, result['Message'])
-        __dn = result['Value']
-        if not __dn:
-          raise WErr(500, 'No DN found for %s@%s' % (username, group))
+        result = getDNForUsernameInGroup(user, group)
+        if not result['OK'] or not result.get('Value'):
+          raise WErr(500, '%s@%s has no registred DN: %s' % (user, group, result.get('Message') or "")
+        
         if voms:
-          result = yield self.threadTask(ProxyManagerClient().downloadVOMSProxy, __dn, group, requiredVOMSAttribute=Registry.getVOForGroup(group),
-                                         requiredTimeLeft=proxyLifeTime)
+          result = yield self.threadTask(ProxyManagerClient().downloadVOMSProxy, user, group, requiredTimeLeft=proxyLifeTime)
         else:
-          result = yield self.threadTask(ProxyManagerClient().downloadProxy, __dn, group, requiredTimeLeft=proxyLifeTime)
+          result = yield self.threadTask(ProxyManagerClient().downloadProxy, user, group, requiredTimeLeft=proxyLifeTime)
         if not result['OK']:
           raise WErr(500, result['Message'])
         self.log.notice('Proxy was created.')
