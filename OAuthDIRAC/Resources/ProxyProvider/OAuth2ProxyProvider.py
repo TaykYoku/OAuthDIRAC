5602b93718489bcd550196fec9b9a1f83e14827c
TaykYoku <yokutayk@gmail.com>
2020-06-05 00:05:18 +0200

    fix bug

--- OAuthDIRAC/Resources/ProxyProvider/OAuth2ProxyProvider.py
+++ OAuthDIRAC/Resources/ProxyProvider/OAuth2ProxyProvider.py
@@ -67,7 +67,7 @@ class OAuth2ProxyProvider(ProxyProvider):
         if result['OK']:
           return S_OK({'Status': 'ready'})
     return S_OK({'Status': 'needToAuth', 'Comment': 'Need to auth with %s identity provider',
-                 'Action': {'openURL': {'URL': '%s/auth/%s' % (getAuthAPI().strip('/'), idP)}})
+                 'Action': {'openURL': {'URL': '%s/auth/%s' % (getAuthAPI().strip('/'), idP)}}})
 
     # self.oauth2 = OAuth2(self.parameters['IdProvider'])
     # # TODO: Get reserved session for IDs and IdP
